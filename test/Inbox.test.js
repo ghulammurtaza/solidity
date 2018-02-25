8c1ee8f4454c2e990754e10c6e6660a01d872d26
ghulammurtaza <ghulam.murtaza@coeus-solutions.de>
2018-02-25 07:02:21 +0500

    section2 lecture 42- fetching accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,14 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); //web3 require needs to call through constructor function
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
-  web3.eth.getAccounts()
-      .then(fetchedAccounts => {
-        console.log(fetchedAccounts);
-      });
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts()
+
 });
 
 describe('Inbox', () => {
   it('deploys a new contract', () => {
+    console.log(accounts);
   });
 });
